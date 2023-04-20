c3fb2a5752b81a4a6e6529b3780dd7bf0e3ecd9a
ym-shakhsey <131234317+ym-shakhsey@users.noreply.github.com>
2023-04-20 22:37:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,26 +43,30 @@ except URLError as e:
   streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 
-#streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-realted functions
+def get_fruit_load_list():
+    with  my_cnx.cursor() as my_cur
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List')
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
-streamlit.header("")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thank you for adding ', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+    return "Thank you for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
